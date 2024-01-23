c86724e2451bb8705721228c3e9b5e8f1a0002bd
VanyaLyashuk <ivan129522@gmail.com>
2024-01-23 15:47:51 +0200

    Update search bar styles

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -2,12 +2,12 @@ const SearchBar = () => {
   return (
     <form className="flex drop-shadow-md">
       <input
-        className="w-full p-2 px-3 text-sm border rounded-l-md sm:text-base"
+        className="w-full px-3 py-[10px] text-base leading-relaxed border rounded-none rounded-bl-md rounded-tl-md"
         type="text"
         placeholder="Search City"
       />
       <button
-        className="px-6 py-2 text-sm font-medium leading-normal text-white transition-colors bg-neutral-800 rounded-r-md sm:text-base hover:bg-orange-600 focus:bg-orange-600 active:bg-orange-600"
+        className="px-6 py-2 text-base font-medium leading-normal text-white transition-colors bg-neutral-800 rounded-r-md hover:bg-orange-600 focus:bg-orange-600 active:bg-orange-600"
         type="submit"
       >
         Search
