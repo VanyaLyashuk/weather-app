1083a13aa2edd79462fbb30430088006e4263fbc
VanyaLyashuk <ivan129522@gmail.com>
2024-01-23 20:43:39 +0200

    Refine search bar UI, update shadow

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -1,13 +1,13 @@
 const SearchBar = () => {
   return (
-    <form className="flex drop-shadow-md">
+    <form className="flex ">
       <input
-        className="w-full px-3 py-[10px] text-base leading-relaxed border rounded-none rounded-bl-md rounded-tl-md"
+        className="w-full px-3 py-[10px] text-base leading-relaxed rounded-none rounded-bl-md rounded-tl-md shadow-md"
         type="text"
         placeholder="Search City"
       />
       <button
-        className="px-6 py-2 text-base font-medium leading-normal text-white transition-colors bg-neutral-800 rounded-r-md hover:bg-primary focus:bg-primary active:bg-primary"
+        className="px-6 py-2 text-base font-medium leading-normal text-white transition-colors shadow-md bg-neutral-800 rounded-r-md hover:bg-primary focus:bg-primary active:bg-primary"
         type="submit"
       >
         Search
