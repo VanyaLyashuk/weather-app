df6cf421f86f45a04b5e3cf04cb6ec2be2088567
VanyaLyashuk <ivan129522@gmail.com>
2024-01-25 09:37:51 +0200

    style(input): adjust left padding on md breakpoint

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -2,7 +2,7 @@ const SearchBar = () => {
   return (
     <form className="flex ">
       <input
-        className="w-full px-3 py-[10px] text-base leading-relaxed rounded-none rounded-bl-md rounded-tl-md shadow-md md:text-[18px]"
+        className="w-full px-3 py-[10px] text-base leading-relaxed rounded-none rounded-bl-md rounded-tl-md shadow-md md:text-[18px] md:pl-[18px]"
         type="text"
         placeholder="Search City"
       />
