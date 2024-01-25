34924e28fd9ad33b586ffca489baf81c2f450fd4
VanyaLyashuk <ivan129522@gmail.com>
2024-01-25 09:25:24 +0200

    Increase font size for search button and input field

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -2,12 +2,12 @@ const SearchBar = () => {
   return (
     <form className="flex ">
       <input
-        className="w-full px-3 py-[10px] text-base leading-relaxed rounded-none rounded-bl-md rounded-tl-md shadow-md"
+        className="w-full px-3 py-[10px] text-base leading-relaxed rounded-none rounded-bl-md rounded-tl-md shadow-md md:text-[18px]"
         type="text"
         placeholder="Search City"
       />
       <button
-        className="px-6 py-2 text-base font-medium leading-normal text-white transition-colors shadow-md bg-neutral-800 rounded-r-md hover:bg-primary focus:bg-primary active:bg-primary"
+        className="px-6 py-2 text-base font-medium leading-normal text-white transition-colors shadow-md bg-neutral-800 rounded-r-md hover:bg-primary focus:bg-primary active:bg-primary md:text-[18px]"
         type="submit"
       >
         Search
