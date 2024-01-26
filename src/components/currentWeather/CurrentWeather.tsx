6c985da88ab144d0afe2db70597b8edf11c06142
VanyaLyashuk <ivan129522@gmail.com>
2024-01-26 10:37:45 +0200

    style(currentWeather): update styles for xs, sm, md breakpoints

--- src/components/currentWeather/CurrentWeather.tsx
+++ src/components/currentWeather/CurrentWeather.tsx
@@ -6,7 +6,7 @@ const CurrentWeather = () => {
   return (
     <section className="px-3 py-6 mb-3 rounded-md shadow-md sm:mb-4 sm:px-4 sm:py-6 sm:flex sm:flex-wrap md:px-[18px] md:py-8">
       <div className="flex items-center justify-between w-full">
-        <p className="text-primary md:text-lg sm:text-lg">Jan 4, 01:04pm</p>
+        <p className="text-primary md:text-lg">Jan 4, 01:04pm</p>
         <ul className="sm:flex sm:items-center sm:gap-4">
           <li className="flex gap-2 text-sm sm:text-base">
             <span className="uppercase text-zinc-500">Sunrise</span>
@@ -18,21 +18,21 @@ const CurrentWeather = () => {
           </li>
         </ul>
       </div>
-      <h1 className="w-full text-[28px] leading-none font-bold break-words mt-2 sm:mt-1 sm:text-[34px] md:text-4xl">
+      <h1 className="w-full text-[28px] leading-none font-bold break-words mt-2 sm:mt-1 sm:text-3xl md:text-4xl">
         Izmail, UA
       </h1>
-      <div className="flex items-center gap-3 sm:pr-5 md:pr-8">
+      <div className="flex items-center gap-3 sm:pr-4 md:pr-8">
         <img
-          className="w-[140px] mx-[-34px] my-[-16px] sm:w-[150px] sm:my-0 md:w-[160px]"
+          className="w-[140px] mx-[-34px] my-[-16px] sm:my-[-8px] md:w-[160px]"
           src={weatherImg}
           alt="weather"
         />
         <p className="text-5xl">12°C</p>
       </div>
-      <p className="mb-4 text-lg font-medium sm:mb-0 sm:text-xl md:text-[22px] sm:order-last sm:w-full">
-        Feels like 11°C. Overcast clouds. Moderate breeze
+      <p className="mb-4 text-lg font-medium sm:text-xl sm:mb-0 md:text-[22px] sm:order-last sm:w-full">
+        Feels like 11°C. Overcast clouds. <span className="text-nowrap">Moderate breeze</span>
       </p>
-      <ul className="border-l-[3px] border-primary pl-3 py-1 flex items-center gap-x-3 gap-y-2 flex-wrap text-base sm:text-lg sm:pl-5 sm:w-[calc(100%_-_235px)] sm:self-center md:text-xl md:pl-8 md:gap-x-4">
+      <ul className="border-l-[3px] border-primary pl-3 py-1 flex items-center gap-x-3 gap-y-2 flex-wrap text-base sm:text-lg sm:pl-4 sm:w-[calc(100%_-_200px)] sm:self-center md:text-xl md:pl-8 md:gap-x-4 md:w-[calc(100%_-_235px)]">
         <li className="flex items-center gap-1">
           <img src={arrowIcon} alt="wind" />
           <span>6.1m/s WSW</span>
