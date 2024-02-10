094f3436df3a249fd545cb2a9627708d08ecd9fa
VanyaLyashuk <ivan129522@gmail.com>
2024-02-10 15:20:05 +0200

    style(Forecast): update margins and paddings

--- src/components/forecast/Forecast.tsx
+++ src/components/forecast/Forecast.tsx
@@ -6,6 +6,10 @@ const Forecast = () => {
       <h2 className="text-xl font-bold mb-3 sm:text-[22px] md:text-2xl">3-hour Forecast 5 days</h2>
       <ul className='grid gap-3 md:gap-4'>
         <ForecastItem />
+        <ForecastItem />
+        <ForecastItem />
+        <ForecastItem />
+        <ForecastItem />
       </ul>
     </section>
   );
