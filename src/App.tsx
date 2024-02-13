b3c2373ed53fbef7f21f1ba983fd5b7d12dcaa97
VanyaLyashuk <ivan129522@gmail.com>
2024-02-13 20:07:55 +0200

    Refactor: Extract header into a separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,14 @@
-import { useEffect, useState } from "react";
 import CurrentWeather from "./components/currentWeather/CurrentWeather";
 import Footer from "./components/footer/Footer";
 import Forecast from "./components/forecast/Forecast";
+import Header from "./components/header/Header";
 import SearchBar from "./components/searchBar/SearchBar";
 
-type Theme = "light" | "dark";
 
 const App = () => {
-  const [theme, setTheme] = useState<Theme>("light");
-
-  useEffect(() => {
-    const root = window.document.documentElement;
-    root.classList.remove("dark", "light");
-    root.classList.add(theme);
-  }, [theme]);
-
-  const onThemeChange = (btn: string): void => {
-    if (btn === theme) return;
-
-    setTheme(btn === "light" ? "light" : "dark");
-  };
-
   return (
     <div className="container flex flex-col min-h-screen pt-3 md:pt-4">
-      <header className="flex justify-start mb-3 md:mb-4">
-        <div className="font-medium text-neutral-600 dark:text-slate-400">
-          <span className=""></span>Theme:{" "}
-          <button
-            className={theme === "dark" ? "text-primary" : ""}
-            onClick={() => onThemeChange("dark")}
-          >
-            Dark
-          </button>{" "}
-          /{" "}
-          <button
-            className={theme === "light" ? "text-primary" : ""}
-            onClick={() => onThemeChange("light")}
-          >
-            Light
-          </button>
-        </div>
-      </header>
+      <Header />
       <SearchBar />
       <main>
         <CurrentWeather />
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+type Theme = "light" | "dark";
+
+const Header = () => {
+  const [theme, setTheme] = useState<Theme>("light");
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    root.classList.remove("dark", "light");
+    root.classList.add(theme);
+  }, [theme]);
+
+  const onThemeChange = (btn: string): void => {
+    if (btn === theme) return;
+
+    setTheme(btn === "light" ? "light" : "dark");
+  };
+
+  return (
+    <header className="flex justify-start mb-3 md:mb-4">
+      <div className="font-medium text-neutral-600 dark:text-slate-400">
+        <span className=""></span>Theme:{" "}
+        <button
+          className={theme === "dark" ? "text-primary" : ""}
+          onClick={() => onThemeChange("dark")}
+        >
+          Dark
+        </button>{" "}
+        /{" "}
+        <button
+          className={theme === "light" ? "text-primary" : ""}
+          onClick={() => onThemeChange("light")}
+        >
+          Light
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
