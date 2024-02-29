c5ccb4afcaed1be34da4f96c63f14a80c613c8c0
VanyaLyashuk <ivan129522@gmail.com>
2024-02-29 12:18:31 +0200

    style(CurrentWeather): update icon size, icon margins and font sizes

--- src/components/currentWeather/CurrentWeather.tsx
+++ src/components/currentWeather/CurrentWeather.tsx
@@ -36,33 +36,33 @@ const CurrentWeather: React.FC = () => {
   return (
     <section className="px-3 py-6 mb-6 rounded-md shadow-md sm:px-4 sm:py-6 sm:flex sm:flex-wrap md:px-[18px] md:py-8 sm:mb-7 bg-white dark:bg-slate-800">
       <div className="flex items-center justify-between w-full">
-        <p className="text-primary md:text-lg">{date}</p>
+        <p className="text-primary current-weather-date-xs">{date}</p>
         <ul className="sm:flex sm:items-center sm:gap-4">
-          <li className="flex gap-2 text-sm sm:text-base">
+          <li className="flex gap-2 text-sm sunrise-sunset-xs">
             <span className="uppercase text-zinc-500">Sunrise</span>
             <span className="dark:text-slate-400">{sunrise}</span>
           </li>
-          <li className="flex gap-2 text-sm sm:text-base">
+          <li className="flex gap-2 text-sm sunrise-sunset-xs">
             <span className="text-gray-500 uppercase">Sunset</span>
             <span className="dark:text-slate-400">{sunset}</span>
           </li>
         </ul>
       </div>
-      <h1 className="w-full text-[28px] leading-none font-bold break-words mt-2 sm:mt-1 sm:text-3xl md:text-4xl dark:text-slate-400">
+      <h1 className="w-full text-3xl leading-none font-bold break-words mt-2 sm:mt-2 sm:text-[32px] md:text-4xl dark:text-slate-400">
         {locationName}
       </h1>
       <div className="flex items-center gap-3 sm:pr-4 md:pr-8">
         <img
-          className="w-[140px] mx-[-34px] my-[-16px] sm:my-[-8px] md:w-[160px]"
+          className="w-[120px] mx-[-18px] my-[-16px] sm:my-[-8px] md:w-[160px]"
           src={icon}
           alt="weather"
         />
         <p className="text-5xl dark:text-slate-400">{temperature}</p>
       </div>
-      <p className="mb-4 text-lg font-medium sm:text-xl sm:mb-0 md:text-[22px] sm:order-last sm:w-full dark:text-slate-400">
+      <p className="mb-4 text-lg font-medium sm:text-xl sm:mb-0 md:text-2xl sm:order-last sm:w-full dark:text-slate-400 current-weather-description-xs">
         Feels like {feelsLike}°C. {description}
       </p>
-      <ul className="border-l-[3px] border-primary pl-3 py-1 flex items-center gap-x-3 gap-y-2 flex-wrap text-base sm:text-lg sm:pl-4 sm:w-[calc(100%_-_200px)] sm:self-center md:text-xl md:pl-8 md:gap-x-4 md:w-[calc(100%_-_235px)] dark:text-slate-400">
+      <ul className="border-l-[3px] border-primary pl-3 py-1 flex items-center gap-x-3 gap-y-2 flex-wrap text-base current-weather-condition-xs sm:pl-4 sm:w-[calc(100%_-_200px)] sm:self-center md:text-xl md:pl-8 md:gap-x-4 md:w-[calc(100%_-_245px)] dark:text-slate-400">
         <li className="flex items-center gap-1">
           <ArrowIcon />
           <span>{wind}</span>
