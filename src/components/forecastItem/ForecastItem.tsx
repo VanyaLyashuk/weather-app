697a4b0edc8cc9bb7a3bc641c3bffb18f31a21c1
VanyaLyashuk <ivan129522@gmail.com>
2024-02-29 14:10:07 +0200

    style(ForecastItem): update icon size, margins, paddings and font sizes

--- src/components/forecastItem/ForecastItem.tsx
+++ src/components/forecastItem/ForecastItem.tsx
@@ -35,37 +35,37 @@ const ForecastItem: React.FC<IForecastItemProps> = ({ forecast, index }) => {
         onClick={handleDetails}
         className="flex items-center justify-between w-full px-3 rounded-md sm:px-4 md:px-[18px]"
       >
-        <p className="sm:text-lg">{forecast.date}</p>
-        <div className="flex items-center">
-          <img src={icon} className="w-12 sm:w-14 md:w-15" alt="clouds" />
-          <p className="sm:text-lg">{`${temperatureMax}/${temperatureMin}°C`}</p>
+        <p className="text-left min-w-[95px] forecast-text-size-xs forecast-btn-date-width-xs">{forecast.date}</p>
+        <div className="flex items-center min-w-[100px] forecast-temp-width-xs">
+          <img src={icon} className="w-10 forecast-btn-icon-size-xs md:w-15" alt="clouds" />
+          <p className="forecast-text-size-xs">{`${temperatureMax}/${temperatureMin}°C`}</p>
         </div>
         <TriangleIcon showDetails={showDetails} />
       </button>
       <div
-        className={`px-3 sm:px-4 sm:grid sm:grid-cols-[1fr_3px_160px] md:grid-cols-[1fr_3px_180px] md:px-[18px] sm:gap-x-4 md:gap-x-6 transition-all duration-[400ms] ${detailsClasses}`}
+        className={`px-3 sm:px-4 sm:grid sm:grid-cols-[1fr_3px_auto] md:px-[18px] sm:gap-x-4 md:gap-x-6 transition-all duration-[400ms] ${detailsClasses}`}
       >
         <div className="flex items-center gap-3 sm:mt-[-14px] sm:pt-2 md:pt-3 sm:col-span-1 ">
           <img
-            className="w-[100px] mx-[-22px] sm:w-[120px] sm:mx-[-26px] sm:my-[-12px]"
+            className="w-[100px] mx-[-12px] sm:w-[116px] sm:m-[-12px]"
             src={icon}
             alt="weather"
           />
           <div>
-            <p className="text-base font-bold sm:text-lg md:text-xl">
+            <p className="text-base font-bold forecast-descr-text-size-xs md:text-2xl">
               {description}
             </p>
-            <p className="text-base md:text-xl">
-              The high will be {`${temperatureMax}°C`},{" "}
-              <span className="whitespace-nowrap sm:whitespace-normal">
-                the low will be {`${temperatureMin}°C`}
+            <p className="text-base forecast-text-size-xs md:text-xl">
+              The high will be {`${temperatureMax}°C`}, the low {" "}
+              <span className="whitespace-nowrap">
+                will be {`${temperatureMin}°C`}
               </span>
               .
             </p>
           </div>
         </div>
         <div className="hidden sm:block w-[3px] sm:bg-primary sm:row-span-2"></div>
-        <ul className="mb-2 border-l-[3px] border-primary pl-3 py-1 flex items-center gap-x-3 gap-y-2 flex-wrap text-base md:text-lg sm:row-span-2 sm:mb-0 sm:gap-y-1 md:gap-y-2 sm:pl-0 sm:border-l-0">
+        <ul className="mb-2 border-l-[3px] border-primary pl-3 py-1 flex items-center gap-x-3 gap-y-2 flex-wrap text-base forecast-text-size-xs sm:grid sm:row-span-2 sm:mb-0 sm:gap-y-1 md:gap-y-2 sm:pl-0 sm:border-l-0">
           <li className="flex items-center gap-1">
             <ArrowIcon />
             <span>{wind}</span>
@@ -81,57 +81,57 @@ const ForecastItem: React.FC<IForecastItemProps> = ({ forecast, index }) => {
             <span>{visibility}</span>
           </li>
         </ul>
-        <div className="overflow-auto md:text-lg sm:col-span-1 sm:overflow-hidden">
+        <div className="overflow-auto forecast-time-period-text-size-xs sm:col-span-1 sm:overflow-hidden md:text-lg">
           <table className="text-center">
             <thead>
               <tr>
                 <td></td>
-                <td className="p-1 sm:pb-[6px] sm:px-[6px] md:px-3 sm:pt-0 md:pb-3 text-center">
+                <td className="p-1.5 sm:pb-[6px] sm:px-[6px] md:px-4 sm:pt-0 md:pb-3 text-center">
                   Morning
                 </td>
-                <td className="p-1 sm:pb-[6px] sm:px-[6px] md:px-3 sm:pt-0 md:pb-3 text-center">
+                <td className="p-1.5 sm:pb-[6px] sm:px-[6px] md:px-4 sm:pt-0 md:pb-3 text-center">
                   Afternoon
                 </td>
-                <td className="p-1 sm:pb-[6px] sm:px-[6px] md:px-3 sm:pt-0 md:pb-3 text-center">
+                <td className="p-1.5 sm:pb-[6px] sm:px-[6px] md:px-4 sm:pt-0 md:pb-3 text-center">
                   Evening
                 </td>
-                <td className="p-1 sm:pb-[6px] sm:px-[6px] md:px-3 sm:pt-0 md:pb-3 text-center">
+                <td className="p-1.5 pr-0 sm:pb-[6px] sm:pr-0 sm:px-[6px] md:px-4 sm:pt-0 md:pb-3 text-center md:pr-0">
                   Night
                 </td>
               </tr>
             </thead>
             <tbody>
               <tr>
-                <td className="text-sm text-left uppercase text-zinc-500 md:text-base">
+                <td className="text-sm text-left uppercase text-zinc-500 forecast-time-period-label-size-xs md:text-base md:pr-4">
                   Temperature
                 </td>
-                <td className="px-1 sm:px-[6px] md:px-3 text-center">
+                <td className="px-1 sm:px-[6px] md:px-4 text-center">
                   {morning.temperature}
                 </td>
-                <td className="px-1 sm:px-[6px] md:px-3 text-center">
+                <td className="px-1 sm:px-[6px] md:px-4 text-center">
                   {afternoon.temperature}
                 </td>
-                <td className="px-1 sm:px-[6px] md:px-3 text-center">
+                <td className="px-1 sm:px-[6px] md:px-4 text-center">
                   {evening.temperature}
                 </td>
-                <td className="px-1 sm:px-[6px] md:px-3 text-center">
+                <td className="pl-1 sm:pl-[6px] md:pl-4 text-center">
                   {night.temperature}
                 </td>
               </tr>
               <tr>
-                <td className="text-sm text-left uppercase text-zinc-500 md:text-base">
+                <td className="text-sm text-left uppercase text-zinc-500 forecast-time-period-label-size-xs md:text-base md:pr-4">
                   Feels like
                 </td>
-                <td className="px-1 sm:sm:px-[6px] md:px-3 text-center">
+                <td className="px-1 sm:sm:px-[6px] md:px-4 text-center">
                   {morning.feelsLike}
                 </td>
-                <td className="px-1 sm:sm:px-[6px] md:px-3 text-center">
+                <td className="px-1 sm:sm:px-[6px] md:px-4 text-center">
                   {afternoon.feelsLike}
                 </td>
-                <td className="px-1 sm:sm:px-[6px] md:px-3 text-center">
+                <td className="px-1 sm:sm:px-[6px] md:px-4 text-center">
                   {evening.feelsLike}
                 </td>
-                <td className="px-1 sm:sm:px-[6px] md:px-3 text-center">
+                <td className="pl-1 sm:sm:pl-[6px] md:pl-4 text-center">
                   {night.feelsLike}
                 </td>
               </tr>
