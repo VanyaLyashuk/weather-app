9e67ee0b98a9fecf3a60ee1e0c243506fdf7f1f4
VanyaLyashuk <ivan129522@gmail.com>
2024-03-01 15:41:34 +0200

    Refacrot CurrentWeather layout from flex to grid

--- src/components/currentWeather/CurrentWeather.tsx
+++ src/components/currentWeather/CurrentWeather.tsx
@@ -34,8 +34,8 @@ const CurrentWeather: React.FC = () => {
   } = currentWeather;
 
   return (
-    <section className="px-3 py-6 mb-6 rounded-md shadow-md sm:px-4 sm:py-6 sm:flex sm:flex-wrap md:px-[18px] md:py-8 sm:mb-7 bg-white dark:bg-slate-800">
-      <div className="flex items-center justify-between w-full">
+    <section className="px-3 py-6 mb-6 rounded-md shadow-md sm:px-4 sm:py-6 sm:grid sm:grid-cols-[auto,1fr] md:px-[18px] md:py-8 sm:mb-7 bg-white dark:bg-slate-800">
+      <div className="flex items-center justify-between w-full sm:col-span-2">
         <p className="text-primary current-weather-date-xs">{date}</p>
         <ul className="sm:flex sm:items-center sm:gap-4">
           <li className="flex gap-2 text-sm sunrise-sunset-xs">
@@ -48,7 +48,7 @@ const CurrentWeather: React.FC = () => {
           </li>
         </ul>
       </div>
-      <h1 className="w-full text-3xl leading-none font-bold break-words mt-2 sm:mt-2 sm:text-[32px] md:text-4xl dark:text-slate-400">
+      <h1 className="w-full text-3xl leading-none font-bold break-words mt-2 sm:mt-2 sm:text-[32px] md:text-4xl dark:text-slate-400 sm:col-span-2">
         {locationName}
       </h1>
       <div className="flex items-center gap-3 sm:pr-4 md:pr-8">
@@ -59,10 +59,10 @@ const CurrentWeather: React.FC = () => {
         />
         <p className="text-5xl dark:text-slate-400">{temperature}</p>
       </div>
-      <p className="mb-4 text-lg font-medium sm:text-xl sm:mb-0 md:text-2xl sm:order-last sm:w-full dark:text-slate-400 current-weather-description-xs">
+      <p className="mb-4 text-lg font-medium sm:text-xl sm:mb-0 md:text-2xl sm:order-last sm:w-full dark:text-slate-400 current-weather-description-xs sm:col-span-2">
         Feels like {feelsLike}°C. {description}
       </p>
-      <ul className="border-l-[3px] border-primary pl-3 py-1 flex items-center gap-x-3 gap-y-2 flex-wrap text-base current-weather-condition-xs sm:pl-4 sm:w-[calc(100%_-_200px)] sm:self-center md:text-xl md:pl-8 md:gap-x-4 md:w-[calc(100%_-_245px)] dark:text-slate-400">
+      <ul className="w-full border-l-[3px] border-primary pl-3 py-1 flex items-center gap-x-3 gap-y-2 flex-wrap text-base current-weather-condition-xs sm:pl-4 sm:self-center md:text-xl md:pl-8 md:gap-x-4 dark:text-slate-400">
         <li className="flex items-center gap-1">
           <ArrowIcon />
           <span>{wind}</span>
