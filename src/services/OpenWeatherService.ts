603f048b1e36869692dca83394a191b0211191a8
VanyaLyashuk <ivan129522@gmail.com>
2024-03-04 09:19:49 +0200

    refactor(OpenWeatherService): enhance URL base reuse and extract base image URL

--- src/services/OpenWeatherService.ts
+++ src/services/OpenWeatherService.ts
@@ -17,7 +17,8 @@ import {
 } from "./weatherUtils";
 
 class OpenWeatherService {
-  private _apiBase: string = "https://api.openweathermap.org/data/2.5/";
+  private _apiBase: string = "https://api.openweathermap.org/";
+  private _apiBaseImg: string = "http://openweathermap.org/img/wn/";
   private _apiKey: string = "54e6a33b161001dd37822697dce2955c";
   private _defaultLocation: ILocation = {
     lat: 45.34,
@@ -33,7 +34,7 @@ class OpenWeatherService {
   }
 
   public async getCurrentWeather(): Promise<ITransformedCurrentWeather> {
-    const url: string = `${this._apiBase}weather?lat=${this._defaultLocation.lat}&lon=${this._defaultLocation.lon}&appid=${this._apiKey}&units=metric`;
+    const url: string = `${this._apiBase}data/2.5/weather?lat=${this._defaultLocation.lat}&lon=${this._defaultLocation.lon}&appid=${this._apiKey}&units=metric`;
     const res: ICurrentWeather = await this.getResource<ICurrentWeather>(url);
 
     return this._transformWeatherData(res);
@@ -42,7 +43,7 @@ class OpenWeatherService {
   public async getFiveDayForecast(): Promise<
     ITransformedForecastWithSummary[]
   > {
-    const url: string = `${this._apiBase}forecast?lat=${this._defaultLocation.lat}&lon=${this._defaultLocation.lon}&appid=${this._apiKey}&units=metric`;
+    const url: string = `${this._apiBase}data/2.5/forecast?lat=${this._defaultLocation.lat}&lon=${this._defaultLocation.lon}&appid=${this._apiKey}&units=metric`;
     const res: IFiveDayForecast = await this.getResource<IFiveDayForecast>(url);
 
     return this._transformFiveDayForecast(res.list).splice(0, 5);
@@ -56,7 +57,7 @@ class OpenWeatherService {
       date: _transformDate(new Date(data.dt * 1000), "mm dd, time"),
       sunrise: _transformTime(new Date(data.sys.sunrise * 1000)),
       sunset: _transformTime(new Date(data.sys.sunset * 1000)),
-      icon: `http://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`,
+      icon: `${this._apiBaseImg}${data.weather[0].icon}@4x.png`,
       temperature: `${Math.round(data.main.temp)}°C`,
       feelsLike: Math.round(data.main.feels_like),
       pressure: `${data.main.pressure}hPa`,
@@ -78,7 +79,7 @@ class OpenWeatherService {
         data: [
           {
             time: _transformTime(new Date(item.dt * 1000)),
-            icon: `http://openweathermap.org/img/wn/${item.weather[0].icon}@4x.png`,
+            icon: `${this._apiBaseImg}${item.weather[0].icon}@4x.png`,
             temperature: `${Math.round(item.main.temp)}°C`,
             feelsLike: `${Math.round(item.main.feels_like)}°C`,
             temperatureMin: Math.round(item.main.temp_min),
