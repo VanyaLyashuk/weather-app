799704ff2c479fec85fd4a7cedaddb0f3e9962fa
VanyaLyashuk <ivan129522@gmail.com>
2024-03-04 23:11:10 +0200

    feat(App): integrate coords state and update props for weather components

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
+import React, { useState } from "react";
 import CurrentWeather from "./components/currentWeather/CurrentWeather";
 import ErrorBoundary from "./components/errorBoundary/ErrorBoundary";
 import Footer from "./components/footer/Footer";
 import Forecast from "./components/forecast/Forecast";
 import Header from "./components/header/Header";
 import SearchBar from "./components/searchBar/SearchBar";
+import { ILocation } from "./models";
 
-const App = () => {
+const App: React.FC = () => {
+  const [searchedCityCoords, setSearchedCityCoords] = useState<ILocation>({
+    lat: 45.34,
+    lon: 28.84,
+  });
+
+  const onCitySelect = (coords: ILocation): void => {
+    setSearchedCityCoords(coords);
+  };
+
+  const {lat, lon} = searchedCityCoords;
+
   return (
     <div className="container flex flex-col min-h-screen pt-3 md:pt-4">
       <Header />
-      <SearchBar />
+      <SearchBar onCitySelect={onCitySelect} />
       <main>
         <ErrorBoundary>
-          <CurrentWeather />
+          <CurrentWeather lat={lat} lon={lon}/>
         </ErrorBoundary>
         <ErrorBoundary>
-          <Forecast />
+          <Forecast lat={lat} lon={lon}/>
         </ErrorBoundary>
       </main>
       <Footer />
