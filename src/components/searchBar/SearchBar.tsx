47f1faf4b2e12c0d64310812d1d2f96dd23455c1
VanyaLyashuk <ivan129522@gmail.com>
2024-03-04 09:39:33 +0200

    feat(SearchBar): implement controlled component behaviour

--- src/components/searchBar/SearchBar.tsx
+++ src/components/searchBar/SearchBar.tsx
@@ -1,10 +1,26 @@
-const SearchBar = () => {
+import React, { useState } from "react";
+
+const SearchBar: React.FC = () => {
+  const [search, setSearch] = useState<string>("");
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setSearch(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+
+    console.log(search);
+  };
+
   return (
-    <form className="flex mb-3 md:mb-4">
+    <form className="flex mb-3 md:mb-4" onSubmit={handleSubmit}>
       <input
         className="w-full px-3 py-[10px] text-base leading-relaxed rounded-none rounded-bl-md rounded-tl-md shadow-md sm:text-lg md:pl-[18px] dark:bg-slate-800 dark:text-slate-400"
         type="text"
+        onChange={handleSearchChange}
         placeholder="Search City"
+        value={search}
       />
       <button
         className="px-6 py-2 text-base font-medium leading-normal text-white transition-colors shadow-md bg-neutral-800 rounded-r-md hover:bg-primary focus:bg-primary active:bg-primary md:text-[18px] dark:bg-slate-400 dark:hover:bg-primary dark:focus:bg-primary dark:active:bg-primary dark:text-slate-800 dark:hover:text-white"
