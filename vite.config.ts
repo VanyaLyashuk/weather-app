6fedd809cc05568515cd0b74f1c74e37c41265de
VanyaLyashuk <ivan129522@gmail.com>
2024-03-09 09:42:03 +0200

    chore: update build and deploy config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/weather-app/',
   resolve: {
     alias: {
       '#root': __dirname,
