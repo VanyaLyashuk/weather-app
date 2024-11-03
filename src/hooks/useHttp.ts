e356b5bab99cb223fd39090b1ceb2e1abbba0225
VanyaLyashuk <ivan129522@gmail.com>
2024-11-03 14:29:49 +0200

    refactor(useHttp): update useHttp hook for serverless fetchWeatherData integration

--- src/hooks/useHttp.ts
+++ src/hooks/useHttp.ts
@@ -1,31 +1,32 @@
 import { useCallback, useState } from "react";
-import { IHttpRequestOptions } from "../models";
 
 export const useHttp = () => {
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
 
   const request = useCallback(
-    async <T>(
-      url: string,
-      {
-        method = "GET",
-        body = null,
-        headers = { accept: "application/json" },
-      }: IHttpRequestOptions
+    async <T,>(
+      endpoint: string,
+      params: string
     ): Promise<T> => {
       setLoading(true);
 
       try {
-        const response = await fetch(url, { method, body, headers });
+        const response = await fetch("/.netlify/functions/fetchWeatherData", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ endpoint, params }),
+        });
 
         if (!response.ok) {
-          throw new Error(`Could not fetch ${url}, status: ${response.status}`);
+          setError("Error: Could not fetch data from the server");
+          throw new Error(`Status: ${response.status}`);
         }
 
         const data: T = await response.json();
         setLoading(false);
-        
         return data;
       } catch (e: any) {
         setLoading(false);
@@ -35,6 +36,8 @@ export const useHttp = () => {
     },
     []
   );
+
   const clearError = useCallback(() => setError(null), []);
+
   return { loading, request, error, clearError };
 };
